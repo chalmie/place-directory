327d589095b9ee5512f470c7619326aeeceda3b3
Blair Peterson and Hillary Hintzen <hillary07@gmail.com>
2016-01-25 11:27:08 -0800

    update html and css write new scripts for click and submit functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,32 @@
-function Place(city,landmark,timeOfYear) {
+function Place(city,landmark,timeOfYear,notes) {
   this.city = city;
   this.landmark = landmark;
   this.timeOfYear = timeOfYear;
-  this.notes = [];
+  this.notes = notes;
 }
 
-$ document.ready(function (){
+$ (document).ready(function() {
   $("form#new-place").submit(function(event){
+    event.preventDefault();
+
     var userCity = $("input#new-city").val();
     var userLandmark = $("input#new-landmark").val();
     var userTimeOfYear = $("input#new-time-of-year").val();
     var userNotes = $("input#new-notes").val();
+    var newPlace = new Place(userCity,userLandmark,userTimeOfYear,userNotes);
+
+  $("ul#output").append("<li><span class='city'>" + newPlace.city + "</span></li>")
   });
 
-}};
+  $("#show-place").last().click(function() {
+    $("#show-place").show();
+    $(".city").text(newPlace.city);
+    $(".landmark").text(newPlace.landmark);
+    $(".timeOfYear").text(newPlace.timeOfYear);
+    $(".notes").text(newPlace.notes);
+  });
+
+
+
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,9 @@
 describe('Place', function (){
   it("creates a new place with the given specifications", function() {
-    var testPlace = new Place ("Amsterdam", "Vondelpark", "Summer");
+    var testPlace = new Place ("Amsterdam", "Vondelpark", "Summer", "Notes");
     expect(testPlace.city).to.equal("Amsterdam");
     expect(testPlace.landmark).to.equal("Vondelpark");
     expect(testPlace.timeOfYear).to.equal("Summer");
-    expect(testPlace.notes).to.eql([]);
+    expect(testPlace.notes).to.equal("Notes");
   });
 });

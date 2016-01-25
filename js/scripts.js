18b539dbe6f77190ce96defaac6e91e3b2e61113
Blair Peterson and Hillary Hintzen <hillary07@gmail.com>
2016-01-25 10:26:33 -0800

    create page user layout and write jQuery for form

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,13 @@ function Place(city,landmark,timeOfYear) {
   this.timeOfYear = timeOfYear;
   this.notes = [];
 }
+
+$ document.ready(function (){
+  $("form#new-place").submit(function(event){
+    var userCity = $("input#new-city").val();
+    var userLandmark = $("input#new-landmark").val();
+    var userTimeOfYear = $("input#new-time-of-year").val();
+    var userNotes = $("input#new-notes").val();
+  });
+
+}};
